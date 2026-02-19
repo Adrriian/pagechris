0e091023d07873f8c1f624bfc341d59be912bd07
Adrian Raul Ribeiro <135435739+Adrriian@users.noreply.github.com>
2026-02-19 20:40:30 -0300

    asdasd

---
+++ app/page.tsx
@@ -0,0 +1,30 @@
+
+
+export default function Home() {
+  return (
+    <div className="w-screen h-screen bg-cover container bg-[url('https://i.ibb.co/gMB4KNV3/7-Creative-Construction-Birthday-Party-Themes-That-Are-Sure-to-Impress.jpg')]" >
+      <div className="bg-black/50 w-screen h-screen">
+      <div className="p-5 shadow-xl ">
+        <div>
+          <h1 className="font-bold text-3xl">Constru Chris</h1>
+        </div>
+        <div className="mt-20 gap-10 flex flex-col w-70">
+          <div>
+            <h1 className="text-yellow-500 text-sm font-bold">CONSTRUÇÃO E REFORMAS</h1>
+          </div>
+          <div>
+            <h1 className="text-2xl">Construímos com qualidade hoje para você viver tranquilo amanhã</h1>
+          </div>
+          <div>
+            <h1 className="text-sm text-gray-300">Obras residenciais e comerciais com compromisso, prazo e acabamento profissional.</h1>
+          </div>
+          <div className="bg-orange-600 text-center rounded-2xl p-2 cursor-pointer w-60">
+            <a href="https://wa.me/5548988422966?text=Olá%20quero%20mais%20informações" target="_blank" className="">Faça um orçamento agora</a>
+          </div>
+        </div>
+      </div>
+      </div>
+    </div>
+   
+  );
+}
